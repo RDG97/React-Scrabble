096e9cba107f356beaccaa8482b7156bd333fdb9
RDG97 <ryangann1997@gmail.com>
2023-01-19 00:30:59 +0000

    Set up react. got board concept down

---
+++ scrabble/src/Board.js
@@ -0,0 +1,16 @@
+export default function Board(props) {
+    return(
+    <div className="d-inline-flex p-3 bg-secondary text-white">
+        <div className="row mx-1">
+            <div className="container tile bg-warning mx-1 my-1"><p>test</p></div>
+            <div className="container tile bg-warning mx-1 my-1"><p>test</p></div>
+            <div className="container tile bg-warning mx-1 my-1"><p>test</p></div>
+        </div>
+        <div className="row mx-1">
+            <div className="container tile bg-warning mx-1 my-1"><p>test</p></div>
+            <div className="container tile bg-warning mx-1 my-1"><p>test</p></div>
+            <div className="container tile bg-warning mx-1 my-1"><p>test</p></div>
+        </div>
+    </div>
+    )
+}
