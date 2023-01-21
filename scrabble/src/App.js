c8d933a797adeae55682f570d16122a7743147f9
RDG97 <ryangann1997@gmail.com>
2023-01-21 23:26:16 +0000

    started adding tiles and their points. (tilefile.js)

--- scrabble/src/App.js
+++ scrabble/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Board from './Board';
-import Board2 from './Board2';
+import tiles from './tilefile.js'
+import './tilefile'
 
 function App() {
+
+
+  
+
+
+
+  console.log(tiles)
+
   return (
     <div className="App">
       <center>
       <Board />
-      <Board2 />
       </center>
     </div>
   );
